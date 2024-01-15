cc1e66bd65139476cbbbf5efe4ae1419bd921077
danymukesha <danymukesha@gmail.com>
2024-01-15 17:32:53 +0100

    rearrange tree

--- ensembl_variant_lookup/__init__.py
+++ ensembl_variant_lookup/__init__.py
@@ -4,4 +4,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from ensembl_variant_lookup import routes
+from ensembl_variant_lookup import fetch_variants
--- ensembl_variant_lookup/routes.py
+++
@@ -1,9 +0,0 @@
-# ensembl_variant_lookup/routes.py
-
-from flask import render_template
-
-from ensembl_variant_lookup import app
-
-@app.route('/')
-def index():
-    return render_template('index.html')
